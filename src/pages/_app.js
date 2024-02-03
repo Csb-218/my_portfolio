1ac57cfcb41a0d3743bfd2773098980fa3a12ef6
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-02-04 00:16:18 +0530

    ... initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
+
+import Navbar from "@/components/navigation/Navbar";
+import { NavProvider } from "@/context/NavContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </NavProvider>
+    </>
+  )
+
+    ;
 }
