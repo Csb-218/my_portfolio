cc4c55ce05de03a670182c27389e124bc68da1e2
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-02-04 14:28:30 +0530

    fix : fixed toggle of navbar

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useNav } from '@/context/NavContext'
@@ -6,13 +6,13 @@ import { useNav } from '@/context/NavContext'
 const Navbar = () => {
 
     const { setSection, section, setPage, page } = useNav()
-
+    const [show,setShow] = useState(false)
 
     const router = useRouter()
 
     return (
         <>
-            <nav className=" backdrop-blur-md bg-transparent dark:bg-gray-900 fixed w-full z-50 top-0 start-0  dark:border-gray-600">
+            <nav className=" backdrop-blur-md bg-transparent  fixed w-full z-50 top-0 start-0  dark:border-gray-600">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <Image src="/me.png" className="rounded-full h-10 w-10 object-cover" alt="my Logo" width={100} height={100} />
@@ -30,22 +30,24 @@ const Navbar = () => {
                         >
                             Contact
                         </button>
-                        <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                        <button 
+                          onClick={()=>setShow(!show)}
+                         type="button"
+                          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
                         </button>
                     </div>
-                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <div className={`items-center justify-between  w-full md:flex md:w-auto md:order-1 ${show?"flex":"hidden"}`} >
+                        <ul className="flex flex-col w-full p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <button
-                                    className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                    className="block py-2 px-3 rounded  md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700  dark:border-gray-700"
                                     onClick={() => {
                                         setSection('home')
                                         router?.pathname !== '/' && router.push('/')
-
                                     }}
                                 >
                                     Home
@@ -53,7 +55,7 @@ const Navbar = () => {
                             </li>
                             <li>
                                 <button
-                                    className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                    className="block py-2 px-3  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700  dark:border-gray-700"
                                     onClick={() => {
                                         setSection('about')
                                         router?.pathname !== '/' && router.push('/')
@@ -64,11 +66,11 @@ const Navbar = () => {
                                 </button>
                             </li>
                             {/* <li>
-                                <a href="#" className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                                <a href="#" className="block py-2 px-3  rounded  md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700  dark:border-gray-700">Services</a>
                             </li> */}
                             <li>
                                 <button
-                                    className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                    className="block py-2 px-3 rounded  md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700  dark:border-gray-700"
                                     onClick={() => {
 
                                         setSection('projects')
